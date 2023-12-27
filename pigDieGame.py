8ccb53c3b6a47f2950f187c05a191b1eca9bd539
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-27 15:46:07 -0800

    add max_score display at end of game

--- pigDieGame.py
+++ pigDieGame.py
@@ -28,7 +28,8 @@ player_scores = [0 for _ in range(players)]
 while max(player_scores) < max_score:
 
   for player_idx in range(players):
-    print("\nPlayer number", player_idx + 1, "turn has just started!\n")
+    print("\nPlayer number", player_idx + 1, "your turn has just started!\n")
+    print("Your total score is:", player_scores[player_idx], "\n")
     current_score = 0
 
     while True:
@@ -49,3 +50,7 @@ while max(player_scores) < max_score:
     
     player_scores[player_idx] += current_score
     print("Your total score is:", player_scores[player_idx])
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1, "is the winner with a score of:", max_score)
