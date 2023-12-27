973fdd51148a31111bc180479db24390b144c419
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-27 15:06:23 -0800

    add conditional checking player input validation with error handling

--- pigDieGame.py
+++ pigDieGame.py
@@ -7,5 +7,17 @@ def roll():
 
   return roll
 
-value = roll()
-print(value)
+# This checks if user input is a valid number of players
+while True:
+  players = input("Enter the number of players (2 - 4): ")
+  # isdigit will tell us if it's a valid whole number or not
+  if players.isdigit():  
+    # Converts user input string into a integer
+    players = int(players)
+    if 2 <= players <= 4:
+      break
+    else:
+      print("Must be between 1 -4 players.")
+  else:
+    print("Invalid, try again.")
+
