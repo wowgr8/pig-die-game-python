0e05b94e759b7884b764a059dfff6af93f8409a5
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-27 15:22:09 -0800

    add while loop simulating turn until max score is reached, player decides to stay, or a 1 is rolled

--- pigDieGame.py
+++ pigDieGame.py
@@ -21,3 +21,31 @@ while True:
   else:
     print("Invalid, try again.")
 
+max_score = 50
+# list comprehension - puts 0 inside of list for every player we have
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+
+  for player_idx in range(players):
+    print("\nPlayer number", player_idx + 1, "turn has just started!\n")
+    current_score = 0
+
+    while True:
+      should_roll = input("Would you like to roll (y)? ")
+      if should_roll.lower() != "y":
+        break
+
+      value = roll()
+      if value == 1:
+        print("You rolled a 1! Your turn is over!")
+        current_score: 0
+        break
+      else: 
+        current_score += value
+        print("You rolled a:", value)
+
+      print("Your score is:", current_score)
+    
+    player_scores[player_idx] += current_score
+    print("Your total score is:", player_scores[player_idx])
